ce740d1579f10c8f80e54f964448ed21823ce14a
Vigi73 <vigi73@mail.ru>
2018-09-24 12:54:58 +0700

    Добавлен поиск2

--- m_blog/views.py
+++ m_blog/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.db.models import Q
 
+
 def post_list(request):
     search_query = request.GET.get('search', '')
     if search_query:
         posts = Post.objects.filter(Q(title__contains=search_query) | Q(text__contains=search_query))
     else:
         posts = Post.objects.all()
-
     return render(request, 'm_blog/post_list.html', {'posts': posts})
 
 
