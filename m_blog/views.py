d1fe16e846afc7cfab8b47c69a24b36dff9a3a3e
Vigi73 <vigi73@mail.ru>
2018-09-24 12:32:08 +0700

    Добавлен поиск

--- m_blog/views.py
+++ m_blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, get_object_or_404
-from .models import *
-
+from .models import Post
+from django.db.models import Q
 
 def post_list(request):
-    posts = Post.objects.all()
+    search_query = request.GET.get('search', '')
+    if search_query:
+        posts = Post.objects.filter(Q(title__contains=search_query) | Q(text__contains=search_query) | Q(date_create__day=str(search_query)))
+    else:
+        posts = Post.objects.all()
+
     return render(request, 'm_blog/post_list.html', {'posts': posts})
 
 
