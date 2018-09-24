cbaa9ebd2c0f5263cb620b3b68bf0ca8e1440c63
Vigi73 <vigi73@mail.ru>
2018-09-24 10:08:12 +0700

    add detail_post

--- m_blog/urls.py
+++ m_blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list_url'),
+    path('post/<pk>/', views.post_detail, name='post_detail_url'),
 
 ]
--- m_blog/views.py
+++ m_blog/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 
 def post_list(request):
     posts = Post.objects.all()
-
     return render(request, 'm_blog/post_list.html', {'posts': posts})
 
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'm_blog/post_detai.html', {'post': post})
