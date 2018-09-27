1539ffa0a23f2188567f6fa362f09eff00cbe5a9
Vigi73 <vigi73@mail.ru>
2018-09-27 15:32:05 +0700

    paginators

--- m_blog/views.py
+++ m_blog/views.py
@@ -1,15 +1,45 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
 from django.db.models import Q
 
 
 def post_list(request):
+
     search_query = request.GET.get('search', '')
     if search_query:
         posts = Post.objects.filter(Q(title__contains=search_query) | Q(text__contains=search_query))
+
     else:
         posts = Post.objects.all()
-    return render(request, 'm_blog/post_list.html', {'posts': posts})
+
+    paginator = Paginator(posts, 2)
+
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = f'?page={page.previous_page_number()}'
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = f'?page={page.next_page_number()}'
+    else:
+        next_url = ''
+
+    context = {
+            'page_object': page,
+            'is_paginated': is_paginated,
+            'next_url': next_url,
+            'prev_url': prev_url,
+
+        }
+
+    return render(request, 'm_blog/post_list.html', context=context)
+
 
 
 def post_detail(request, pk):
