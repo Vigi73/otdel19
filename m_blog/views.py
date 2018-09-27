a3a38e8af1fe8ff63e560c6690425d1217145891
Vigi73 <vigi73@mail.ru>
2018-09-27 16:08:24 +0700

    paginators1

--- m_blog/views.py
+++ m_blog/views.py
@@ -13,7 +13,7 @@ def post_list(request):
     else:
         posts = Post.objects.all()
 
-    paginator = Paginator(posts, 2)
+    paginator = Paginator(posts, 5)
 
     page_number = request.GET.get('page', 1)
     page = paginator.get_page(page_number)
--- sert/views.py
+++ sert/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
-from .models import Sert_post
 
+from django.core.paginator import Paginator
+from .models import Sert_post
 # Create your views here.
 
 def sert_list(request):
-    sert_lists = Sert_post.objects.all()
-    return render(request, 'sert/sert.html', {'sert_lists': sert_lists})
+    posts = Sert_post.objects.all()
+
+    paginator = Paginator(posts, 5)
+
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = f'?page={page.previous_page_number()}'
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = f'?page={page.next_page_number()}'
+    else:
+        next_url = ''
+
+    context = {
+        'page_object1': page,
+        'is_paginated1': is_paginated,
+        'next_url1': next_url,
+        'prev_url1': prev_url
+
+    }
+
+    return render(request, 'sert/sert.html', context=context)
