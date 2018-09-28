5458c0945b5d5101e773115ddcaa76bd99d0307f
Vigi73 <vigi73@mail.ru>
2018-09-28 15:28:14 +0700

    guest_book11

--- guest_book/models.py
+++ guest_book/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class Post_guest(models.Model):
+    g_author = models.ForeignKey(User, verbose_name='Автор', on_delete=models.CASCADE)
+    g_title = models.CharField(verbose_name='Заголовок', max_length=250)
+    g_text = RichTextField(verbose_name='Контент сообщения')
+    g_date_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    class Meta:
+       ordering = ['-g_date_create']
+
+    def __str__(self):
+        return self.g_title
